9622ee108026cb4c45e648978309f38291a68c92
judy200234 <judy200234@gmail.com>
2016-05-22 17:58:04 +0800

    source code - main class(open this system)

---
+++ parkpaysystemOut/GUICheckOut.java
@@ -0,0 +1,8 @@
+ public class GUICheckOut { 
+    public static void main(String[] args) { 
+		
+		EncryptorGUICheckOut gui = new EncryptorGUICheckOut();	
+		gui.runMain();
+		new EncryptorGUICheckOut().start();
+    }
+}
