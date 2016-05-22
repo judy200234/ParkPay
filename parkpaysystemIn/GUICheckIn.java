8d45647c6357fb6fb9cbdd56acad0013f4880012
judy200234 <judy200234@gmail.com>
2016-05-22 17:50:52 +0800

    source code - main class(open this system)

---
+++ parkpaysystemIn/GUICheckIn.java
@@ -0,0 +1,8 @@
+ public class GUICheckIn { 
+    public static void main(String[] args) { 
+		
+		EncryptorGUICheckIn gui = new EncryptorGUICheckIn();	
+		gui.runMain();
+		new EncryptorGUICheckIn().start();
+    }
+}
